383d87864e367acc2dc0f3242de2f59f57be8848
Srinimahi <163630503+Srinimahi@users.noreply.github.com>
2024-06-16 21:04:54 +0530

    Update dicejs.js

--- dicejs.js
+++ dicejs.js
@@ -1,52 +1,52 @@
 
 var randomNumber1=Math.ceil(Math.random()*6);
 if (randomNumber1=== 1){
-  document.querySelector(".im1").setAttribute('src','../The Dice Game/Dicee+Challenge+-+Starting+Files/images/dice1.png');
+  document.querySelector(".im1").setAttribute('src','dice1.png');
 }
 else if(randomNumber1===2){
     
-  document.querySelector(".im1").setAttribute('src','../The Dice Game/Dicee+Challenge+-+Starting+Files/images/dice2.png');
+  document.querySelector(".im1").setAttribute('src','dice2.png');
 }
 else if(randomNumber1===3){
   
-  document.querySelector(".im1").setAttribute('src','../The Dice Game/Dicee+Challenge+-+Starting+Files/images/dice3.png');
+  document.querySelector(".im1").setAttribute('src','dice3.png');
 }
 else if(randomNumber1===4){
   
-  document.querySelector(".im1").setAttribute('src','../The Dice Game/Dicee+Challenge+-+Starting+Files/images/dice4.png');
+  document.querySelector(".im1").setAttribute('src','dice4.png');
 }
 else if(randomNumber1===5){
   
-  document.querySelector(".im1").setAttribute('src','../The Dice Game/Dicee+Challenge+-+Starting+Files/images/dice5.png');
+  document.querySelector(".im1").setAttribute('src','dice5.png');
 }
 else {
   
-  document.querySelector(".im1").setAttribute('src','../The Dice Game/Dicee+Challenge+-+Starting+Files/images/dice6.png');
+  document.querySelector(".im1").setAttribute('src','dice6.png');
 }
 
 var randomNumber2=Math.ceil(Math.random()*6);
 if (randomNumber2=== 1){
-  document.querySelector(".im2").setAttribute('src','../The Dice Game/Dicee+Challenge+-+Starting+Files/images/dice1.png');
+  document.querySelector(".im2").setAttribute('src','dice1.png');
 }
 else if(randomNumber2===2){
     
-  document.querySelector(".im2").setAttribute('src','../The Dice Game/Dicee+Challenge+-+Starting+Files/images/dice2.png');
+  document.querySelector(".im2").setAttribute('src','dice2.png');
 }
 else if(randomNumber2===3){
   
-  document.querySelector(".im2").setAttribute('src','../The Dice Game/Dicee+Challenge+-+Starting+Files/images/dice3.png');
+  document.querySelector(".im2").setAttribute('src','dice3.png');
 }
 else if(randomNumber2===4){
   
-  document.querySelector(".im2").setAttribute('src','../The Dice Game/Dicee+Challenge+-+Starting+Files/images/dice4.png');
+  document.querySelector(".im2").setAttribute('src','dice4.png');
 }
 else if(randomNumber2===5){
   
-  document.querySelector(".im2").setAttribute('src','../The Dice Game/Dicee+Challenge+-+Starting+Files/images/dice5.png');
+  document.querySelector(".im2").setAttribute('src','dice5.png');
 }
 else {
   
-  document.querySelector(".im2").setAttribute('src','../The Dice Game/Dicee+Challenge+-+Starting+Files/images/dice6.png');
+  document.querySelector(".im2").setAttribute('src','dice6.png');
 }
 if(randomNumber1>randomNumber2){
   document.querySelector(".headd").innerHTML='Player 1 Wins'
